67f524bd75e79fdcc432b9e6f49cc78ab60d7abd
zhaniadilya <zhaniadilya@gmail.com>
2023-01-21 18:13:41 +0600

    title методы құрылды

--- demo.java
+++ demo.java
@@ -2,4 +2,8 @@ public classs demo{
     public static void main(String[] args){
         System.out.println("Salem,Alem!");
     }
+
+    public static void title() {
+        System.out.println("TITLE");
+    }
 }
