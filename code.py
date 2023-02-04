5e8681c98e7d24b233525beffb870d629a2aa9f6
zhaniadilya <zhaniadilya@gmail.com>
2023-02-04 09:10:25 +0600

    3-лаб 4 есебі

--- code.py
+++ code.py
@@ -20,10 +20,21 @@ else:
     print('a men b manderi ten!')"""
 
 #3-tapsyrma
-a = int(input('a = '))
+"""a = int(input('a = '))
 b = int(input('b = '))
 if(a>b):
     for i in range(a-(a+1)%2, b-b%2, -2):
         print(i, end=' ')
 else:
-    print("a = b nemese a < b")
+    print("a = b nemese a < b")"""
+
+#4-tapsyrma
+n = int(input('N = '))
+k = 0
+num = 0 
+for i in range(1, n+1):
+    k+=i
+print("Bar kartalardyn manin engiziniz: ")
+for i in range(n-1):
+    k -= int(input())
+print("Zhogalgan karta: ", k)
