bf24474edcfa37e8ff6d7255d74b9cf08b9c9d4f
zhaniadilya <zhaniadilya@gmail.com>
2023-02-04 08:21:48 +0600

    3-лаб 2 есебі

--- code.py
+++ code.py
@@ -1,7 +1,20 @@
-a = int(input('a = '))
+#1-tapsyrma
+"""a = int(input('a = '))
 b = int(input('b = '))
 if a <= b:
     for i in range(a, b+1):
         print(i, end=' ')
 else:
-    print('a men b manderin qaita engiziniz, a >= b!')        
+    print('a men b manderin qaita engiziniz, a >= b!')"""
+
+#2-tapsyrma
+a = int(input('a = '))
+b = int(input('b = '))
+if a < b:
+    for i in range(a, b+1):
+        print(i, end=' ')
+elif a > b:
+    for i in reversed(range(b, a+1)):
+        print(i, end=' ')
+else:
+    print('a men b manderi ten!')
